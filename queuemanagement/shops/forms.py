acda187be8bb4de71f69e15accff50a2db43ff6d
RitikaMoolya <moolyaritika.@gmail.com>
2024-09-22 13:25:11 +0530

    Promotional Images added

--- queuemanagement/shops/forms.py
+++ queuemanagement/shops/forms.py
@@ -4,4 +4,4 @@ from . import models
 class CreateShop(forms.ModelForm): 
     class Meta: 
         model = models.Shop
-        fields = ['name','bio','contact','address','timings','image','category']
+        fields = ['name','category','bio','contact','address','timings','image','upload_promotional_images_1','upload_promotional_images_2','upload_promotional_images_3','upload_promotional_images_4']
--- queuemanagement/shops/models.py
+++ queuemanagement/shops/models.py
@@ -14,12 +14,17 @@ class Category(models.Model):
 class Shop(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shops')
     name = models.CharField(max_length=75)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
     bio = models.TextField()
     contact = models.CharField(max_length=15)
     address = models.TextField()
     timings = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/', null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+    upload_promotional_images_1=models.ImageField(upload_to="images/", null=True)
+    upload_promotional_images_2=models.ImageField(upload_to="images/", null=True)
+    upload_promotional_images_3=models.ImageField(upload_to="images/", null=True)
+    upload_promotional_images_4=models.ImageField(upload_to="images/", null=True)
+    
 
     def __str__(self):
         return self.name
