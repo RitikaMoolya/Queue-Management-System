f57e873f56c60c756da862aa2a7f565639c714c9
Navin Karkera <navin@opencraft.com>
2024-09-22 10:32:09 +0530

    feat: book appointment

--- queuemanagement/shops/models.py
+++ queuemanagement/shops/models.py
@@ -1,28 +1,29 @@
-from django.db import models
 from django.contrib.auth.models import User
-
+from django.db import models
 
 # Create your models here.
+
 
 class Category(models.Model):
     category_title = models.CharField(max_length=20)
 
     def __str__(self):
-            return self.category_title
+        return self.category_title
+
 
 class Shop(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="shops")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shops')
     name = models.CharField(max_length=75)
     bio = models.TextField()
     contact = models.CharField(max_length=15)
     address = models.TextField()
     timings = models.CharField(max_length=100)
     image = models.ImageField(upload_to='images/', null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
 
     def __str__(self):
         return self.name
-        
+
 
 class Token(models.Model):
     date = models.DateField(auto_now_add=True)
@@ -30,5 +31,3 @@ class Token(models.Model):
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
     number = models.IntegerField()
     current = models.BooleanField()
-
-
--- queuemanagement/shops/views.py
+++ queuemanagement/shops/views.py
@@ -1,45 +1,68 @@
 from datetime import date
-from django.shortcuts import render,redirect
-from .models import Shop, Token, Category
-from . import forms 
-from django.db.models import Max
+
+from django.contrib.auth.decorators import login_required
+from django.db.models import F, Max
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
+
+from . import forms
+from .models import Category, Shop, Token
 
 # Create your views here.
+
 
 def shops_list(request):
     category = request.GET.get('category')
     shops = Shop.objects.all()
     if category:
         category = int(category)
-        shops = shops.filter(category = category)
+        shops = shops.filter(category=category)
     categories = Category.objects.all()
-    return render(request, 'shops/shops_list.html', {'shops': shops, 'categories': categories, 'category':category})
+    return render(request, 'shops/shops_list.html', {'shops': shops, 'categories': categories, 'category': category})
 
-def shop_profile(request,pk):
+
+def shop_profile(request, pk):
     shop = Shop.objects.get(pk=pk)
-    current = shop.token_set.filter(current = True, date = date.today()).first()
-    next_number = shop.token_set.filter(date = date.today()).aggregate(Max('number', default = 0))
-    next_number = next_number['number__max'] + 1
-    return render(request, 'shops/shop_profile.html', {'shop': shop, 'current' : current, 'number': next_number})
+    current = shop.token_set.filter(current=True, date=date.today()).first()
+    your_num = shop.token_set.filter(date=date.today(), user=request.user).first()
+    next_number = shop.token_set.filter(date=date.today()).aggregate(Max('number', default=0))['number__max'] + 1
+    if your_num:
+        your_num = your_num.number
+    return render(
+        request,
+        'shops/shop_profile.html',
+        {
+            'shop': shop,
+            'current': current,
+            'next_number': next_number,
+            'your_num': your_num,
+        },
+    )
+
 
 def shop_new(request):
-    if request.method == 'POST': 
-        form = forms.CreateShop(request.POST, request.FILES) 
+    if request.method == 'POST':
+        form = forms.CreateShop(request.POST, request.FILES)
         if form.is_valid():
-            newshop = form.save(commit=False) 
-            newshop.owner = request.user 
+            newshop = form.save(commit=False)
+            newshop.owner = request.user
             newshop.save()
             return redirect('shops:Shops')
     else:
         form = forms.CreateShop()
-    return render(request, 'shops/shop_new.html', { 'form': form })
+    return render(request, 'shops/shop_new.html', {'form': form})
 
-'''def dashboard(request,pk):
-    shop = Shop.objects.get(pk=pk)
-    return render(request, 'shops/dashboard.html', {'shop': shop} )'''
 
 def dashboard(request):
     return render(request, 'shops/dashboard.html')
 
-    
-    
+
+@login_required
+@require_http_methods(['POST'])
+def book_appointment(request, pk):
+    shop = Shop.objects.get(pk=pk)
+    next_number = shop.token_set.filter(date=date.today()).aggregate(Max('number', default=0))['number__max'] + 1
+    Token.objects.get_or_create(
+        user=request.user, shop=shop, date=date.today(), defaults={'number': next_number, 'current': False}
+    )
+    return redirect('shops:Profile', pk=pk)
